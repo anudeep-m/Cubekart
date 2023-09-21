c2a2f9dd0e88b1be5ee22a474cd6e147ec1654c7
Anudeep Muktinutalapati <m.anudeep2000@gmail.com>
2023-09-21 18:06:58 +0530

    Added prod url in cors urls

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://cubekart.onrender.com"],
+    origin: ["http://localhost:3000", "https://cubekart.onrender.com", "https://cubekart.anudeep.info"],
     credentials: true,
   })
 );
